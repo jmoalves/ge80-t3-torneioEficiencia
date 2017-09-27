9e8c42ab18356d228b8127074f56aedd8d297ca5
Joao Mauricio <jmauricio@pobox.com>
2017-09-27 02:04:31 -0300

    Agregando 12 ciclos

--- api-gen.sh
+++ api-gen.sh
@@ -1,4 +1,15 @@
-cat ciclos/*-*.json \
+maxCiclos=$1
+if [ "a$maxCiclos" == "a" ]; then
+	maxCiclos=12
+fi
+
+cat $(ls -1 ciclos/*-*.json | sort | tail -n $maxCiclos) \
 | jq -s 'reduce .[] as $item ({}; . + $item)' \
 | jq '.' \
 > api/ciclos.json
+
+echo
+echo Agregados $(jq 'length'  api/ciclos.json) ciclos
+jq 'keys' api/ciclos.json
+
+echo
