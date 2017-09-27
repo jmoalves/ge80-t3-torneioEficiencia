bbd9c293f0b6cff2510de9b3ede8942f4b2956e6
Joao Mauricio <jmauricio@pobox.com>
2017-09-27 01:56:00 -0300

    Removendo maxPontos

--- ajusta.sh
+++ ajusta.sh
@@ -2,13 +2,7 @@ pushd ciclos
 for file in *-*.json; do
 	mv $file $file.bkp
 	cat $file.bkp \
-	| sed 's/"patrulha"\: \[/"patrulha": /g' \
-	| sed 's/"idPatrulha"\: \(.*\),/\1: {/g' \
-	| sed 's/"porDia"\: \[/"dia": /g' \
-	| sed 's/"id"\: \(.*\),/\1: {/g' \
-	| sed 's/^[[:blank:]]\+{//g' \
-	| sed 's/]/}/g' \
-	| sed 's/^}$/} }/g' \
+	| sed 's/^.*"maxPontos".*$//g' \
 	| jq '.' \
 	> $file;
 	rm $file.bkp
